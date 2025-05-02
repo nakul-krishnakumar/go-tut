5130e63e3a8cd485e50639f82d9660b9a182c9af
nakul-krishnakumar <nakulkrishnakumar86@gmail.com>
2025-05-02 16:52:30 +0000

    Completed arrays

---
+++ 7_arrays/array.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Array Initialization
+	var nums [4]int
+
+	// Length of the Array
+	fmt.Println("Length: ", len(nums))
+
+	nums[1] = 4
+	// Printing Array
+	fmt.Println("Array: ", nums)
+
+	// Initialize and Declare Array
+	nums2 := [3]int{1, 2, 3}
+	fmt.Println("Second Array: ", nums2)
+
+	// 2D Array
+	nums2D := [3][2]int{{1, 1}, {2, 2}, {3, 3}}
+	fmt.Println("2D Array: ", nums2D)
+
+	// Compile-Time Size Determination
+	nums3 := [...]int{1, 2, 3}
+	fmt.Println(nums3)
+
+
+	//* Arrays are only used when we know the size of the data structure
+	//* For Dynamic Memory Allocation, we use SLICES
+
+	//? advantages of arrays:
+	//?	- fixed size, that is predictable
+	//?	- memory optimization
+	//?	- constant time access
+}
